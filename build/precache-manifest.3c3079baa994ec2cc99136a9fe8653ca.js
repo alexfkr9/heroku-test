79d9f0b1aaef146cd8701668b4c53be172db1f51
Alexfkr9 <alexfkr9@gmail.com>
2021-04-23 15:32:32 +0300

    commit

---
+++ build/precache-manifest.3c3079baa994ec2cc99136a9fe8653ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b3be7847cae59c319e42f89a9038dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e19678177c8d2b6c1472",
+    "url": "/static/js/2.b572e39b.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.b572e39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7ada50b8e042623f37",
+    "url": "/static/js/main.13bb030b.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
